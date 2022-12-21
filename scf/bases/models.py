e50e62d2ba2f2bb8174c2d64f66e9645dff3213f
seeker11 <jmesabong@gmail.com>
2022-12-21 16:15:44 -0500

    Se creo la app inv y se implemento la clase modelo base

--- scf/bases/models.py
+++ scf/bases/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class ClaseModelo(models.Model):
+    estado = models.BooleanField(default=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+    user_crea = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_modifica = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        abstract=True
