386f80bb9950eb7724858ee5f6205c9c0175e9f4
seeker11 <jmesabong@gmail.com>
2022-12-21 16:40:17 -0500

    Se creo el modelo categoria de la app inv y se registro la app en el settings

--- scf/inv/models.py
+++ scf/inv/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from bases.models import ClaseModelo
 
 
 # Create your models here.
-class Categoria(models.Model):
-    pass
+class Categoria(ClaseModelo):
+    descripcion = models.CharField(max_length=100, help_text='Descripción de la categoría', unique=True)
+
+    def __str__(self):
+        return self.descripcion
+
+    def save(self):
+        self.descripcion = self.descripcion.upper()
+        super(Categoria, self).save()
+
+    class Meta:
+        verbose_name_plural = "Categorías"
